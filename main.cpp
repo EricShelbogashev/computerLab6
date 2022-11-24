b481aaab560aeff71abc4456b2ca3620022a77e2
Eric Shelbogashev <e.shelbogashev@gmail.com>
2022-11-24 23:24:40 +0700

    deleted some output

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <libusb-1.0/libusb.h>
-#include <cstdio>
+#include <sstream>
 #include <unordered_map>
 
 using namespace std;
 
+static const int serialNumberLength = 32;
+
+void print_dev(libusb_device *dev);
+
+static unordered_map<int, string> map = {
+        {0,   "Code Missing"},
+        {1,   "Audio device"},
+        {2,   "Network adapter"},
+        {3,   "User Interface Device"},
+        {5,   "Physical Device"},
+        {6,   "Images"},
+        {7,   "Printer"},
+        {8,   "Storage"},
+        {9,   "Concentrator"},
+        {10,  "CDC-Data"},
+        {11,  "Smart Card"},
+        {13,  "Content Security"},
+        {14,  "Video device"},
+        {15,  "Personal Medical Device"},
+        {16,  "Audio and video devices"},
+        {220, "Diagnostic Device"},
+        {224, "Wireless Controller"},
+        {239, "Various Devices"},
+        {254, "Specific device"},
+};
+
 int main() {
+
     libusb_device **devs;
     libusb_context *ctx = nullptr; // libusb session context
     int r; // for results
@@ -15,36 +42,15 @@ int main() {
         cerr << "Error: initialization failed: " << r << endl;
         return 1;
     }
-    // set the verbosity level of debug messages
-    libusb_set_debug(ctx, 3);
     // get a list of all found USB devices
     cnt = libusb_get_device_list(ctx, &devs);
     if (cnt < 0) {
         cerr << "Error: USB device list not received." << endl;
         return 1;
     }
-    libusb_device_descriptor desc{};
+
     for (int i = 0; i < cnt; i++) {
-        libusb_device_handle *handle;
-        auto *data = new uint8_t[33]();
-        r = libusb_get_device_descriptor(devs[i], &desc);
-        if (r < 0) {
-            cerr << "Error: Device handle not received, code: " << r << endl;
-        }
-        printf("%02X\t%02X:%02X  \t",desc.bDeviceClass, desc.idVendor, desc.idProduct);
-        try {
-            libusb_open(devs[i], &handle);
-            if (handle != nullptr) {
-                if (libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, data, 31) >= 0) {
-                    data[32] = '\0';
-                    cout << "Serial Number: \"" << data << "\"";
-                }
-            }
-            libusb_close(handle);
-        } catch (libusb_error &e) {
-            cerr << e << endl;
-        }
-        cout << endl;
+        print_dev(devs[i]);
     }
     // release the memory allocated by the get device list function
     libusb_free_device_list(devs, 1);
@@ -54,3 +60,40 @@ int main() {
 
 // https://www.keil.com/pack/doc/mw/USB/html/_u_s_b__interface__descriptor.html
 // https://libusb.sourceforge.io/api-1.0
+void print_dev(libusb_device *dev) {
+    libusb_device_descriptor desc{};
+    libusb_config_descriptor *config;
+    int r = libusb_get_device_descriptor(dev, &desc);
+    if (r < 0) {
+        cerr << "Error: Device handle not received, code: " << r << endl;
+        return;
+    }
+    libusb_get_config_descriptor(dev, 0, &config);
+    cout << "Device class: " << map[static_cast<int>(desc.bDeviceClass)] << endl;
+    cout << "Vendor id: " << hex << desc.idVendor << endl;
+    cout << "Product id: " << desc.idProduct << endl;
+
+    libusb_device_handle *handle;
+    auto *data = new uint8_t[33]();
+    try {
+        libusb_open(dev, &handle);
+        if (handle != nullptr) {
+            if (libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, data, serialNumberLength - 1) >= 0) {
+                data[serialNumberLength] = '\0';
+                cout << "Serial number: " << data << endl;
+            }
+        }
+    } catch (libusb_error &e) {
+        cerr << e << endl;
+    }
+    delete[] data;
+    if (handle) {
+        try {
+            libusb_close(handle);
+        } catch (libusb_error &e) {
+            cerr << e << endl;
+        }
+    }
+    cout << endl;
+    libusb_free_config_descriptor(config);
+}
